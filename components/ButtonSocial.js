d917e4a4ec06ed53847fea608932063dafd40c83
alisam163 <alisam163@yandex.ru>
2023-01-03 04:23:26 +0300

    edit ButtonSocial

--- components/ButtonSocial.js
+++ components/ButtonSocial.js
@@ -2,9 +2,11 @@ import styles from './ButtonSocial.module.css'
 
 function ButtonSocial(props) {
   const { srcImg, urlSocial = '#' } = props
+
+  const urlSite = 'http://' + urlSocial
   return (
     <div className={styles.buttonSocial}>
-      <a href={urlSocial} target="_blank">
+      <a href={urlSite} target="_blank">
         <img src={srcImg} />
       </a>
     </div>
